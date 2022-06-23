003b302f0a89888ab238a5ca9dd32f96e0db0e1a
SM-Ali <95084592+SM-Ali@users.noreply.github.com>
2022-06-23 08:25:59 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,8 +5,8 @@ class config:
     DATABASE_URL = "postgres://pxlpzgrmujcohs:61f2a8645d65f6eb53926949dda1c9b674d71bdf5fe7bc601b4cbba5ad1d4e11@ec2-23-23-182-238.compute-1.amazonaws.com:5432/dfhded4ocd455q"
     SUDO_USERS = "1433606591" # Sepearted by space.
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = "650764564044-s23f8d24spktppca5s857m4eru91bmhu.apps.googleusercontent.com"
-    G_DRIVE_CLIENT_SECRET = "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"
+    G_DRIVE_CLIENT_ID = "884985649744-smr468udj9rapnaemacqdpj4n8r7bgef.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "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"
     SUPPORT_CHAT_LINK = ""
 
 
